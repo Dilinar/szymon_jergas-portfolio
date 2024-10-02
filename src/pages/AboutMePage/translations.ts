827678fe7fdf47cde7e8a6bee50c5894934925a7
Dilinar <szymonjergas@interia.pl>
2024-10-02 17:38:14 +0200

    Updated CV

--- src/pages/AboutMePage/translations.ts
+++ src/pages/AboutMePage/translations.ts
@@ -44,7 +44,7 @@ export default {
     },
     headerRecomendations: {
         pl: 'Referencje',
-        en: 'Recommendations'
+        en: 'References'
     },
     projects: {
         pl: 'Moje projekty',
